fad68d92a9e147ae397455944c2026adeaebbcad
KRakeesh04 <kanesrakeshan@gmail.com>
2026-05-02 04:07:23 +0530

    add new models and update the model functions

--- ingestion-service/db/model.go
+++ ingestion-service/db/model.go
@@ -1,9 +1,49 @@
 package db
 
 type Device struct {
-	ID          uint   `gorm:"primaryKey" json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	TenantID    uint   `json:"tenant_id"`
-	IsActive    bool   `json:"is_active"`
+	DeviceID  uint    `gorm:"primaryKey" json:"device_id"`
+	TenantID  uint    `json:"tenant_id"`
+	PublicKey *string `json:"public_key"`
+	IsActive  bool    `json:"is_active" gorm:"default:false"`
+	CreatedAt *int64  `json:"created_at"`
+	UpdatedAt *int64  `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+type DeviceDetails struct {
+	DeviceID  uint   `json:"device_id"`
+	TenantID  uint   `json:"tenant_id"`
+	IsActive  bool   `json:"is_active"`
+	CreatedAt *int64 `json:"created_at"`
+	UpdatedAt *int64 `json:"updated_at"`
+}
+
+type DeviceHTTPRegistrationRequest struct {
+	DeviceID uint `json:"device_id"`
+	TenantID uint `json:"tenant_id"`
+}
+
+type DeviceRegistrationRequest struct {
+	PublicKey string `json:"public_key"`
+}
+
+type DeviceRegistrationResponse struct {
+	RegistrationStatus string `json:"registration_status"`
+}
+
+type SensorDeviceData struct {
+	Mode    string  `json:"mode"`
+	VRMS    float64 `json:"v_rms"`
+	TempC   float64 `json:"temp_c"`
+	PeakHz1 int     `json:"peak_hz_1"`
+	PeakHz2 int     `json:"peak_hz_2"`
+	PeakHz3 int     `json:"peak_hz_3"`
+	Status  string  `json:"status"`
+}
+
+type MQTTPayload struct {
+	DeviceID  uint             `json:"device_id"`
+	Timestamp int64            `json:"timestamp"`
+	Nonce     string           `json:"nonce"`
+	Data      SensorDeviceData `json:"data"`
+	Signature string           `json:"signature"`
 }
