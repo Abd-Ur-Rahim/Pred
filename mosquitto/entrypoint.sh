487f0541505573a38a74ae2b285217c3eb475825
KRakeesh04 <kanesrakeshan@gmail.com>
2026-05-01 20:56:19 +0530

    setup Mosquitto broker with authentication and ACL configuration

---
+++ mosquitto/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+DEVICE_USERNAME="${MQTT_DEVICE_USERNAME:-pred-device}"
+DEVICE_PASSWORD="${MQTT_DEVICE_PASSWORD:-dev-device-password}"
+INGESTION_USERNAME="${MQTT_INGESTION_USERNAME:-pred-ingestion}"
+INGESTION_PASSWORD="${MQTT_INGESTION_PASSWORD:-dev-ingestion-password}"
+PASSWORD_FILE="/mosquitto/data/passwords"
+
+if [ -z "$DEVICE_USERNAME" ] || [ -z "$DEVICE_PASSWORD" ] || [ -z "$INGESTION_USERNAME" ] || [ -z "$INGESTION_PASSWORD" ]; then
+  echo "[mosquitto-config] MQTT usernames and passwords must not be empty" >&2
+  exit 1
+fi
+
+echo "[mosquitto-config] writing password file"
+rm -f "$PASSWORD_FILE"
+mosquitto_passwd -b -c "$PASSWORD_FILE" "$DEVICE_USERNAME" "$DEVICE_PASSWORD"
+mosquitto_passwd -b "$PASSWORD_FILE" "$INGESTION_USERNAME" "$INGESTION_PASSWORD"
+chmod 0600 "$PASSWORD_FILE"
+chown mosquitto:mosquitto "$PASSWORD_FILE"
+
+echo "[mosquitto-config] starting broker"
+exec mosquitto -c /mosquitto/config/mosquitto.conf
